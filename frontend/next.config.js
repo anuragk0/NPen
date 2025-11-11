ce116e23395970802dd452c303f6842acf35ab6b
Anurag Kumar <anuragk7028@gmail.com>
2025-11-12 00:44:48 +0530

    Change API destination to production URL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
+        destination: 'https://npen.onrender.com/api/:path*',
       },
     ];
   },
 };
 
 module.exports = nextConfig;
+
